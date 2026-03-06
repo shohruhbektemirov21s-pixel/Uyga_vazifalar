7226692d22a3531c44c65822c35c5af74223fcb7
unknown <shohruhbektemirov21s@gmail.com>
2026-03-07 02:37:01 +0500

    dhh

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php 
+$uqivchi1 = "Ali";
+$uqivchi1bahosi = 5;
+$uqivchi1bahosi1 = 5;
+$uqivchibahosi2 = 5;
+
+$uqivchi2 = "Vali";   
+$uqivchi2bahosi = 4;
+$uqivchi2bahosi1 = 4;   
+$uqivchi2bahosi2 = 4;
+
+$uqivchi3 = "G'ani";    
+$uqivchi3bahosi = 3;    
+$uqivchi3bahosi1 = 3;    
+$uqivchi3bahosi2 = 3;
+
+echo "
+<table style='border:1px solid black; border-collapse:collapse; text-align:center;'>
+
+<tr>
+<th style='border:1px solid black; padding:5px;'>Uqivchi</th>
+<th style='border:1px solid black; padding:5px;'>Bahosi</th>
+<th style='border:1px solid black; padding:5px;'>Bahosi1</th>
+<th style='border:1px solid black; padding:5px;'>Bahosi2</th>
+</tr>
+
+<tr>
+<td>".$uqivchi1."</td>
+<td>".$uqivchi1bahosi."</td>
+<td>".$uqivchi1bahosi1."</td>
+<td>".$uqivchibahosi2."</td>
+</tr>
+
+<tr>
+<td>".$uqivchi2."</td>
+<td>".$uqivchi2bahosi."</td>
+<td>".$uqivchi2bahosi1."</td>
+<td>".$uqivchi2bahosi2."</td>
+</tr>
+
+<tr>
+<td>".$uqivchi3."</td>
+<td>".$uqivchi3bahosi."</td>
+<td>".$uqivchi3bahosi1."</td>
+<td>".$uqivchi3bahosi2."</td>
+</tr>
+
+</table>
+";
+?>
